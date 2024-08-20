ce896e7ad38308e8ed636ca3614a1d1f6ba9cf9c
h0203t <rnjsgurxo12345@gmail.com>
2024-08-20 17:53:21 +0900

    익명 객체

---
+++ src/chap09/Arrony.java
@@ -0,0 +1,13 @@
+package chap09;
+
+public class Arrony {
+//	Perent field = new Perent();
+	Perent field = new Perent() {
+		
+		@Override
+	    void wake() {
+			
+		}
+		
+	};
+}
---
+++ src/chap09/Perent.java
@@ -0,0 +1,11 @@
+package chap09;
+
+public class Perent {
+
+	//필드
+	//생성자
+	//메소드
+	void wake() {
+		System.out.println("7시에 일어남");
+	}
+}
