b6ba65fc4a22ad1193d61188238b09780bbb1694
h0203t <rnjsgurxo12345@gmail.com>
2024-08-21 17:40:30 +0900

    replace

---
+++ src/chap11/StringReplaceExamlple.java
@@ -0,0 +1,13 @@
+package chap11;
+
+public class StringReplaceExamlple {
+
+	public static void main(String[] args) {
+		String str = "가나다가가가가";
+		String newStr = str.replace("가", "하");
+//		System.out.println(str);
+		System.out.println(newStr);
+
+	}
+
+}
