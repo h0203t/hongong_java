96b03c0084edc9cf6cc3d2a4fc79aedcfaf18ed7
h0203t <rnjsgurxo12345@gmail.com>
2024-08-21 10:03:38 +0900

    익명 객체

---
+++ src/chap09/Anony460.java
@@ -0,0 +1,25 @@
+package chap09;
+
+public class Anony460 {
+	Worker field = new Worker() {
+		@Override
+		void start() {
+			System.out.println("디자인을 합니다.");
+		}
+	};
+	
+	void method1() {
+		Worker localVar = new Worker() {
+			
+			@Override
+			void start() {
+				System.out.println("개발을 합니다.");
+			}
+		};
+		localVar.start();
+	}
+	
+	void method2(Worker worker) {
+		worker.start();
+	}
+}
---
+++ src/chap09/AnonyExam.java
@@ -0,0 +1,13 @@
+package chap09;
+
+public class AnonyExam {
+
+	public static void main(String[] args) {
+		 Anony460 anony = new Anony460();
+		 
+		 anony.field.start();
+		 anony.method1();
+//		 anony.method2(Anony460 anony);
+	}
+
+}
